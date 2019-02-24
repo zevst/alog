ebfcf0cc7dad082dc7db2f0f62cb2b0dfb211866
Nikita Koryabkin <Nikita@Koryabk.in>
2019-02-25 00:15:58 +0300

    added linter

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ import (
 
 var configurator sync.Once
 
-// returns ENV variable from environment or .env file as []byte
+// GetEnv returns ENV variable from environment or .env file as []byte
 func GetEnv(key string) []byte {
 	configurator.Do(func() {
 		_ = godotenv.Load()
@@ -22,7 +22,7 @@ func GetEnv(key string) []byte {
 	return []byte(os.Getenv(key))
 }
 
-// returns ENV variable from environment or .env file as string
+// GetEnvStr returns ENV variable from environment or .env file as string
 func GetEnvStr(key string) string {
 	return string(GetEnv(key))
 }
